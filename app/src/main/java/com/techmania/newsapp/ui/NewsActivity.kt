86ac8ce771b3918ea0e9a5d04d3308357d41d6de
Priya Chauhan <priyachauhan.kkr@gmail.com>
2024-09-01 16:58:12 +0530

    packages and fragments are added

--- app/src/main/java/com/techmania/newsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/techmania/newsapp/ui/NewsActivity.kt
@@ -1,10 +1,11 @@
-package com.techmania.newsapp
+package com.techmania.newsapp.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.techmania.newsapp.R
 
 class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
